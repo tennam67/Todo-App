2ee49b3e823f4e6dbe9f5080327dc25840adb53e
Tenzin Namdak <tennam67@gmail.com>
2021-06-03 12:57:37 +0200

    template

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+let items = ["Buy Food", "Cook Food",];
+
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
 
@@ -24,15 +26,17 @@ app.get("/", function(req, res){
   };
 
   let day = today.toLocaleDateString("en-US", options)
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
 app.post("/", (req, res) => {
  let item = req.body.newItem;
+ items.push(item);
  res.redirect("/")
 })
 
+console.log(items)
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
 });
